60433d57ee37d7e23984c9d4a0d7b2cd1343c477
jorge fischer <jorgefisde@gmail.com>
2019-11-22 17:17:11 -0700

    base de datos comit

--- service/database.js
+++ service/database.js
@@ -1,21 +1,22 @@
 const mysql = require('mysql')
 
 const DB_conection = mysql.createConnection({
-    host: 'localhost',
-    user: "root",
-    password: "toor",
+    host: "database-1.c4xpddivvqgu.us-east-2.rds.amazonaws.com",
+    port: "3306",
+    user: "admin",
+    password: "kokielfd",
     database: "Denuncia"
 })
 
-DB_conection.connect((err)=>{
+DB_conection.connect((err) => {
     if (err) {
         console.log("hubo un error al conectar con la base de datos");
         console.log(err);
-        
+
         return
-    }else {
+    } else {
         console.log("conexion establecida");
-        
+
     }
 })
 
