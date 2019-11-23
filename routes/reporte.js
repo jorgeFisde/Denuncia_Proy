113e49ffbec0540ebbbc82e23452f46fca188173
jorge fischer <jorgefisde@gmail.com>
2019-11-22 20:44:40 -0700

    dasda

--- routes/reporte.js
+++ routes/reporte.js
@@ -6,7 +6,7 @@ const multer = require('multer')
 // subir imagenes a un servidor
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './public/Uploads')
+        cb(null, '../public/Uploads')
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname + '-' + Date.now())
