3709622ef1808db0273713d3418934815b9a8f4d
jorge fischer <jorgefisde@gmail.com>
2019-11-24 14:30:41 -0700

    asdsad

--- public/js/cargaHome.js
+++ public/js/cargaHome.js
@@ -1,3 +1,14 @@
+function iniciarMap(){
+    var coord = {lat:-34.5956145 ,lng: -58.4431949};
+    var map = new google.maps.Map(document.getElementById('map'),{
+    zoom: 10,
+    center: coord
+    });
+    var marker = new google.maps.Marker({
+    position: coord,
+    map: map
+    });
+} 
 document.addEventListener('DOMContentLoaded',async ()=>{
 
     var options = {
@@ -28,21 +39,9 @@ document.addEventListener('DOMContentLoaded',async ()=>{
                             <img src="${reporte.fotoURL}" alt="">
                         </div>
                         <div class="col-md-6">
-                            <div id="map">
-
-                            </div>
+                            <div id="map"></div>
                             <script>
-                                function iniciarMap(){
-                                    var coord = {lat:-34.5956145 ,lng: -58.4431949};
-                                    var map = new google.maps.Map(document.getElementById('map'),{
-                                    zoom: 10,
-                                    center: coord
-                                    });
-                                    var marker = new google.maps.Marker({
-                                    position: coord,
-                                    map: map
-                                    });
-                                } 
+                                
                             </script>
                             <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDaeWicvigtP9xPv919E-RNoxfvC-Hqik&callback=iniciarMap"></script>
             
