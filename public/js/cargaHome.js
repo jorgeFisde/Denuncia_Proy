9e8452cfbc167cfb7ca8f89b1c01cad4b1948e85
jorge fischer <jorgefisde@gmail.com>
2019-11-24 14:07:22 -0700

    hhhhhhhhhhh

--- public/js/cargaHome.js
+++ public/js/cargaHome.js
@@ -30,8 +30,23 @@ document.addEventListener('DOMContentLoaded',async ()=>{
                         <div class="col-md-6">
                             <div class="mapouterv ">
                                 <div id="map"
-                                </div>
 
+                                </div>
+                            <script>
+                                function iniciarMap(){
+                                    var coord = {lat:-34.5956145 ,lng: -58.4431949};
+                                    var map = new google.maps.Map(document.getElementById('map'),{
+                                      zoom: 10,
+                                      center: coord
+                                    });
+                                    var marker = new google.maps.Marker({
+                                      position: coord,
+                                      map: map
+                                    });
+                                } 
+                            </script>
+                            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDaeWicvigtP9xPv919E-RNoxfvC-Hqik&callback=iniciarMap"></script>
+                    
                             </div>
                         </div>
                     </div>
@@ -52,21 +67,7 @@ document.addEventListener('DOMContentLoaded',async ()=>{
                     </div>
                 </div>
             </div>
-            <script>
-            ${function iniciarMap(){
-                var coord = {lat:-34.5956145 ,lng: -58.4431949};
-                var map = new google.maps.Map(document.getElementById('map'),{
-                  zoom: 10,
-                  center: coord
-                });
-                var marker = new google.maps.Marker({
-                  position: coord,
-                  map: map
-                });
-            } }
-            </script>
-            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDaeWicvigtP9xPv919E-RNoxfvC-Hqik&callback=iniciarMap"></script>
-
+            
             
             
            
