35cd704526de1cd0dd2a062e32115ce2900b833b
jorge fischer <jorgefisde@gmail.com>
2019-11-24 14:37:56 -0700

    Update cargaHome.js

--- public/js/cargaHome.js
+++ public/js/cargaHome.js
@@ -50,19 +50,19 @@ document.addEventListener('DOMContentLoaded',async ()=>{
                     </div>
                 </div>
             </div>
-            
-            <script>
-                 function iniciarMap(){
-                     var coord = {lat:-34.5956145 ,lng: -58.4431949};
-                     var map = new google.maps.Map(document.getElementById('map'),{
-                        zoom: 10,
-                        center: coord
-                     });
-                        var marker = new google.maps.Marker({
-                        position: coord,
-                        map: map
-                     });
-                 } 
+
+             <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDaeWicvigtP9xPv919E-RNoxfvC-Hqik&callback=iniciarMap">
+             function iniciarMap(){
+                var coord = {lat:-34.5956145 ,lng: -58.4431949};
+                var map = new google.maps.Map(document.getElementById('map'),{
+                   zoom: 10,
+                   center: coord
+                });
+                   var marker = new google.maps.Marker({
+                   position: coord,
+                   map: map
+                });
+            } 
              </script>
             
            
