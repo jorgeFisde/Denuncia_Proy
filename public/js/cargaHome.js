6ad2c549826c047b5447b8edc6231b1537832ebe
jorge fischer <jorgefisde@gmail.com>
2019-11-24 14:12:37 -0700

    dasdas

--- public/js/cargaHome.js
+++ public/js/cargaHome.js
@@ -28,26 +28,24 @@ document.addEventListener('DOMContentLoaded',async ()=>{
                             <img src="${reporte.fotoURL}" alt="">
                         </div>
                         <div class="col-md-6">
-                            <div class="mapouterv ">
-                                <div id="map"
+                            <div id="map">
 
-                                </div>
+                            </div>
                             <script>
                                 function iniciarMap(){
                                     var coord = {lat:-34.5956145 ,lng: -58.4431949};
                                     var map = new google.maps.Map(document.getElementById('map'),{
-                                      zoom: 10,
-                                      center: coord
+                                    zoom: 10,
+                                    center: coord
                                     });
                                     var marker = new google.maps.Marker({
-                                      position: coord,
-                                      map: map
+                                    position: coord,
+                                    map: map
                                     });
                                 } 
                             </script>
                             <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDaeWicvigtP9xPv919E-RNoxfvC-Hqik&callback=iniciarMap"></script>
-                    
-                            </div>
+            
                         </div>
                     </div>
                     <div class="rowForm">
